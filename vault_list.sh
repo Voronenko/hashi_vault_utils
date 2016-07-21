68cec6f99850df0757114bac1d34158bff41fdad
Ubuntu <ubuntu@ip-172-31-25-47.ec2.internal>
2016-07-21 17:02:27 +0000

    list command

---
+++ vault_list.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+VAULT_BASE_URL=${VAULT_ADDR-http://localhost:8200}
+if [ -z $1 ]
+then
+  echo provide path to list keys/subkeys  as the parameter
+  exit 1
+fi
+echo vault list -address=$VAULT_BASE_URL $1
+vault list -address=$VAULT_BASE_URL $1
