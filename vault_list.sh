0c00d50f773ef4a92d47760e651e05339968beb1
Vyacheslav Voronenko <git@voronenko.info>
2018-07-03 14:23:12 +0200

    improve(0.10) aiming 0.10 changes

--- vault_list.sh
+++ vault_list.sh
@@ -5,5 +5,14 @@ then
   echo provide path to list keys/subkeys  as the parameter
   exit 1
 fi
+
+
+if [ -z "$VAULT_API_FAMILY" ]; then
+echo vault policy list -address=$VAULT_BASE_URL $1
+vault policy list -address=$VAULT_BASE_URL $1
+else
 echo vault list -address=$VAULT_BASE_URL $1
 vault list -address=$VAULT_BASE_URL $1
+fi
+
+
--- vault_policy.sh
+++ vault_policy.sh
@@ -2,9 +2,20 @@
 VAULT_BASE_URL=${VAULT_ADDR-http://localhost:8200}
 if [ -z $1 ]
 then
-echo vault policies -address=$VAULT_BASE_URL
-vault policies -address=$VAULT_BASE_URL
+
+if [ -z "$VAULT_API_FAMILY" ]; then
+echo vault policy list -address=$VAULT_BASE_URL
+vault policy list -address=$VAULT_BASE_URL
 else
-echo vault policies -address=$VAULT_BASE_URL $1 
-vault policies -address=$VAULT_BASE_URL $1
+echo vault list -address=$VAULT_BASE_URL
+vault list -address=$VAULT_BASE_URL
+fi
+else
+if [ -z "$VAULT_API_FAMILY" ]; then
+echo vault policy list -address=$VAULT_BASE_URL $1
+vault policy list -address=$VAULT_BASE_URL $1
+else
+echo vault list -address=$VAULT_BASE_URL $1
+vault list -address=$VAULT_BASE_URL $1
+fi
 fi
