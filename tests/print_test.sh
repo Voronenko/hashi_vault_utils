0b8fd9572d4d7fca20b9044a5e8b2a11f088c600
Vyacheslav Voronenko <git@voronenko.info>
2018-07-03 14:10:18 +0200

    improve(0.10) aiming 0.10 changes

---
+++ tests/print_test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+vault_status.sh
+
+vault_init.sh 2>1
+
+vault_auth.sh abfd0e04-7922-6850-e1bd-f02c325f1e2c
+
+vault_write.sh secret/test thetest
+
+vault_read.sh secret/test
+
+vault_policy.sh
+
+vault_list.sh secret/
+
+vault_policy_write.sh demo2 $HOME_DIR/demo/demo.hcl
+
+vault_create_token_with_policy.sh demo2
+
+vault_unseal.sh zz
