913aebc02ea26664f2f5e0b63959f8b5d0d90411
Vyacheslav Voronenko <git@voronenko.info>
2018-07-03 14:26:36 +0200

    improve(0.10) aiming 0.10 changes

--- vault_policy.sh
+++ vault_policy.sh
@@ -12,8 +12,8 @@ vault list -address=$VAULT_BASE_URL
 fi
 else
 if [ -z "$VAULT_API_FAMILY" ]; then
-echo vault policy list -address=$VAULT_BASE_URL $1
-vault policy list -address=$VAULT_BASE_URL $1
+echo vault policy read -address=$VAULT_BASE_URL $1
+vault policy read -address=$VAULT_BASE_URL $1
 else
 echo vault list -address=$VAULT_BASE_URL $1
 vault list -address=$VAULT_BASE_URL $1
